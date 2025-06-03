ecc094241051476828e97d1172cadccf49b0109b
fakesib <fakesibbb@gmail.com>
2025-06-03 13:33:59 +0300

    Test connection

--- src/main/java/com/fakesibwork/hibernate/HibernateStarter.java
+++ src/main/java/com/fakesibwork/hibernate/HibernateStarter.java
@@ -1,7 +1,14 @@
 package com.fakesibwork.hibernate;
 
+import org.hibernate.cfg.Configuration;
+
 public class HibernateStarter {
     public static void main(String[] args) {
-
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        try (var sessionFactory = configuration.buildSessionFactory();
+                var session = sessionFactory.openSession();) {
+            System.out.println("OK");
+        }
     }
 }
